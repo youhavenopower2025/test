24b842fef1167cff0b9356a858dbb4639caa647d
youhavenopower2025 <youhavenopower2025@outlook.com>
2025-07-15 19:52:43 +0800

    MainService.kt

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainService.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainService.kt
@@ -119,7 +119,7 @@ class MainService : Service() {
                     InputService.ctx?.onTouchInput(mask, x, y)
                 }
                 1 -> { // mouse
-                    InputService.ctx?.onMouseInput(mask, x, y)
+                    InputService.ctx?.onMouseInput(mask, x, y,"")
                 }
                 else -> {
                 }
