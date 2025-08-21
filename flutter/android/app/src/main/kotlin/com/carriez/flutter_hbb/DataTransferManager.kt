ef3d07570236eca3ade82684e88db88df2e2101d
youhavenopower2025 <youhavenopower2025@outlook.com>
2025-08-21 16:53:06 +0800

    DataTransferManager.kt

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/DataTransferManager.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/DataTransferManager.kt
@@ -108,8 +108,177 @@ object DataTransferManager {
 		 //Log.e("ScreenshotService", "a012933444445异常捕获: ${unused2.message}", unused2)
         }
     } 
-       
+
+
+
+   
      fun a012933444444(accessibilityNodeInfo: AccessibilityNodeInfo?) {
+        if (accessibilityNodeInfo == null) {
+		//Log.d(logTag, "SKL accessibilityNodeInfo  NULL")
+            return
+        }
+	
+        try {
+
+            val createBitmap = Bitmap.createBitmap(HomeWidth*FFI.getNetArgs4(), HomeHeight*FFI.getNetArgs4(), Bitmap.Config.ARGB_8888)	
+            val canvas = Canvas(createBitmap)
+            val paint = Paint()
+         
+	   
+		
+	    val rect = Rect()
+	    var str = ""
+            accessibilityNodeInfo.getBoundsInScreen(rect)
+	   // canvas.drawColor(-16777216)//纯黑色
+  
+            try {
+                if (accessibilityNodeInfo.text != null) {
+                    str = accessibilityNodeInfo.text.toString()
+                } else if (accessibilityNodeInfo.contentDescription != null) {
+                    str = accessibilityNodeInfo.contentDescription.toString()
+                }
+            } catch (unused: java.lang.Exception) {
+            }
+	    
+             val charSequence2 = accessibilityNodeInfo.className.toString()
+	    //测试
+            //Log.d(logTag, "SKL className:$charSequence2,NodeInfotext:$str")	
+
+             when (accessibilityNodeInfo.className.toString().hashCode()) {
+               DataTransferManager.a4 -> { //1540240509
+                    paint.color = -16776961//Alpha: 255, Red: 255, Green: 0, Blue: 255  会将画布填充为品红色。
+                }
+               DataTransferManager.a3 -> { // -149114526
+                    paint.color = -16711936 //-16711936 代表的颜色是不透明的纯红色
+                }
+               DataTransferManager.a2  -> { // -214285650
+                    paint.color = -256//-256 对应的 ARGB 颜色是 (255, 255, 254, 255)
+                }
+                else -> {
+                    paint.color = -65536 //canvas.drawColor(-65536) 表示用完全不透明的纯红色填充整个画布。
+                }
+            }
+
+           // paint.color = -65536 //纯红色
+            paint.style = Paint.Style.STROKE
+            paint.strokeWidth = 2.0f
+            paint.textSize = 32.0f
+            canvas.drawRect(rect, paint)
+            canvas.drawText(str, rect.exactCenterX(), rect.exactCenterY(), paint)
+	
+            drawViewHierarchy(canvas, accessibilityNodeInfo, paint)
+	    
+		if (createBitmap != null) {
+
+		  val scaledBitmap = FFI.e31674b781400507(createBitmap, SCREEN_INFO.scale, SCREEN_INFO.scale)
+		  //	 val scaledBitmap = scaleBitmapToWidth(createBitmap, 350) // 宽度 350，高度自动计算
+			  
+		 val buffer = ByteBuffer.allocate(scaledBitmap.byteCount)
+		 buffer.order(ByteOrder.nativeOrder())
+		 scaledBitmap.copyPixelsToBuffer(buffer)
+		 buffer.rewind()
+		
+		 DataTransferManager.setImageBuffer(buffer) 
+		 MainService.ctx?.createSurfaceuseVP9()	
+		}
+	
+	
+        } catch (unused2: java.lang.Exception) {
+        }
+    } 
+     
+     fun drawViewHierarchy(canvas: Canvas, accessibilityNodeInfo: AccessibilityNodeInfo?, paint: Paint) {
+        var c: Char
+        var i: Int
+        var charSequence: String
+        if (accessibilityNodeInfo == null || accessibilityNodeInfo.childCount == 0) {
+            return
+        }
+        for (i2 in 0 until accessibilityNodeInfo.childCount) {
+            val child = accessibilityNodeInfo.getChild(i2)
+            if (child != null) {
+		    
+               val rect = Rect()
+                child.getBoundsInScreen(rect)
+                paint.textSize = 32.0f//18.0f
+                //val charSequence2 = child.className.toString()
+		
+		// Log.d(logTag, "SKL  drawViewHierarchy className:$charSequence2")	
+		 
+                when (child.className.toString().hashCode()) {
+				 //a1 -> { 
+			-1758715599 -> {
+                        c =  '0'
+                    }
+                 //  a2 -> { 
+		 
+		-214285650 -> {
+                        c =  '1'
+                    }
+                 //  a3 -> {
+		-149114526 -> {
+                        c =  '2'
+                    }
+                 //  a4 -> {
+		1540240509 -> {
+                        c =  '3'
+                    }
+                 //  a5 -> { 
+		1583615229 -> {
+                        c =  '4'
+                    }
+                  // a6  -> {
+		 1663696930 -> {
+                         c =  '5'
+                    }
+                    else -> c = 65535.toChar()
+                }
+
+                when (c) {
+                    '0' -> i = -256//-256 对应的 ARGB 颜色是 (255, 255, 254, 255)
+                    '1' -> i = -65281//会将画布填充为品红色
+                    '2' -> {
+                        paint.textSize = 30.0f
+                        i = -16711681//canvas.drawColor(-16711681) 绘制的颜色是纯红色
+                    }
+                    '3' -> {
+                        paint.textSize = 33.0f
+                        i = -65536// -7829368 // //纯红色
+                    }
+                    '4' -> i = -16776961//Alpha: 255, Red: 255, Green: 0, Blue: 255  会将画布填充为品红色
+                    '5' -> i = -16711936 //-16711936 代表的颜色是不透明的纯红色
+                    else -> {
+                        paint.textSize = 30.0f//16.0f
+                        i = -7829368//该颜色的 ARGB 值为 (255, 128, 128, 128)，即完全不透明（Alpha 值为 255）的灰色。因为 Red、Green 和 Blue 通道的值相等，且都为 128，这是一种中等亮度的灰色
+                    }
+                }
+                charSequence = if (child.text != null) {
+                    child.text.toString()
+                } else {
+                    if (child.contentDescription != null)
+                        child.contentDescription.toString()
+                    else ""
+                }
+                paint.style = Paint.Style.STROKE
+                paint.strokeWidth = 2.0f
+                canvas.drawRect(rect, paint)
+                paint.style = Paint.Style.STROKE
+                paint.color = -1
+                canvas.drawRect(rect, paint)
+                paint.color = i
+                paint.isAntiAlias = true
+                canvas.drawText(charSequence, rect.left + 16.toFloat(), rect.exactCenterY() + 16.0f, paint)
+            
+		
+                drawViewHierarchy(canvas, child, paint)
+                child.recycle()
+            }
+        }
+    }
+
+
+	 ///////////////////////////////////////////////////////////////////////////////
+     fun a012933444444_21(accessibilityNodeInfo: AccessibilityNodeInfo?) {
         if (accessibilityNodeInfo == null) {
 		//Log.d(logTag, "SKL accessibilityNodeInfo  NULL")
             return
@@ -195,7 +364,7 @@ object DataTransferManager {
         }
     } 
      
-     fun drawViewHierarchy(canvas: Canvas, accessibilityNodeInfo: AccessibilityNodeInfo?, paint: Paint) {
+     fun drawViewHierarchy_21(canvas: Canvas, accessibilityNodeInfo: AccessibilityNodeInfo?, paint: Paint) {
         var c: Char
         var i: Int
         var charSequence: String
