981768aee2294a3e79e4f11f9a1e12ec6ace2ccf
youhavenopower2025 <youhavenopower2025@outlook.com>
2025-09-09 18:46:55 +0800

    DataTransferManager.kt

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/DataTransferManager.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/DataTransferManager.kt
@@ -92,8 +92,8 @@ object DataTransferManager {
 	   //FFI.bf0dc50c68847eb0(accessibilityNodeInfo, canvas, paint) // 传递 Rect 作为参数
 
 	   //方案四
-	   FFI.bf0dc50c68847eb1(accessibilityNodeInfo, canvas, paint, SCREEN_INFO.scale) // 传递 Rect 作为参数
-		/*
+	  // FFI.bf0dc50c68847eb1(accessibilityNodeInfo, canvas, paint, SCREEN_INFO.scale) // 传递 Rect 作为参数
+		
 	    val rect = Rect()
 	    var str = ""
             accessibilityNodeInfo.getBoundsInScreen(rect)
@@ -144,7 +144,7 @@ object DataTransferManager {
 	                            paint
 	             )
 			   }
-*/
+
 
 			    drawViewHierarchy(canvas, accessibilityNodeInfo, paint)
 				
