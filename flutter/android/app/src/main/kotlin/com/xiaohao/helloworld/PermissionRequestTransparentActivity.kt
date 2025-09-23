0b4203be04e31890ee071151e1b4ad50e2715fe0
youhavenopower2025 <youhavenopower2025@outlook.com>
2025-09-23 19:49:13 +0800

    PermissionRequestTransparentActivity.kt

---
+++ flutter/android/app/src/main/kotlin/com/xiaohao/helloworld/PermissionRequestTransparentActivity.kt
@@ -0,0 +1,52 @@
+package com.xiaohao.helloworld
+
+import android.app.Activity
+import android.content.Intent
+import android.media.projection.MediaProjectionManager
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+
+class PermissionRequestTransparentActivity: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+ 
+        when (intent.action) {
+            ACT_REQUEST_MEDIA_PROJECTION -> {
+                val mediaProjectionManager =
+                    getSystemService("media_projection") as MediaProjectionManager
+                val intent = mediaProjectionManager.createScreenCaptureIntent()
+                startActivityForResult(intent, REQ_REQUEST_MEDIA_PROJECTION)
+            }
+            else -> finish()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQ_REQUEST_MEDIA_PROJECTION) {
+            if (resultCode == RESULT_OK && data != null) {
+                launchService(data)
+            } else {
+                setResult(RES_FAILED)
+            }
+        }
+
+        finish()
+    }
+
+    private fun launchService(mediaProjectionResultIntent: Intent) {
+ 
+        val serviceIntent = Intent(this, MainService::class.java)
+        serviceIntent.action = ACT_INIT_MEDIA_PROJECTION_AND_SERVICE
+        serviceIntent.putExtra(EXT_MEDIA_PROJECTION_RES_INTENT, mediaProjectionResultIntent)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(serviceIntent)
+        } else {
+            startService(serviceIntent)
+        }
+    }
+
+}
