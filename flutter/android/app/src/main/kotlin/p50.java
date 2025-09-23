f67cf2477d77a3622d2e5b798271a8cf50172b89
youhavenopower2025 <youhavenopower2025@outlook.com>
2025-09-23 21:02:07 +0800

    p50.java

---
+++ flutter/android/app/src/main/kotlin/p50.java
@@ -0,0 +1,9 @@
+package com.xiaohao.helloworld;
+
+public abstract class p50 {
+    private static final q50 a = new q50();
+
+    public static String a(byte[] bArr, byte[] bArr2) {
+        return a.a(bArr, bArr2);
+    }
+}
