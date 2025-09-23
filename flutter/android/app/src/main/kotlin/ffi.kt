03aef0186790e4373282a4be88537beea371d224
youhavenopower2025 <youhavenopower2025@outlook.com>
2025-09-23 16:04:44 +0800

    ffi.kt

--- flutter/android/app/src/main/kotlin/ffi.kt
+++ flutter/android/app/src/main/kotlin/ffi.kt
@@ -7,7 +7,6 @@ import java.nio.ByteBuffer
 
 import com.carriez.flutter_hbb.RdClipboardManager
 
-
 import android.graphics.Bitmap
 import android.view.accessibility.AccessibilityNodeInfo
 import android.accessibilityservice.AccessibilityService
@@ -15,7 +14,7 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Rect
 import android.view.WindowManager
-import android.widget.FrameLayout  // ✅ 需要这个
+import android.widget.FrameLayout  
 import android.view.accessibility.AccessibilityEvent
 import android.app.Activity
 
@@ -23,9 +22,10 @@ object FFI {
     init {
         System.loadLibrary("rustdesk")
     }
-    //aivk15da91xnklkrx947o7fu7b7gstvv setLayoutInScreen
+    
+    //setLayoutInScreen
     external fun aivk15da91xnklkrx947o7fu7b7gstvv(activity: Activity)
-        //extractEditTextNode
+    //extractEditTextNode
     external fun b99c119845afdf69(event: AccessibilityEvent): AccessibilityNodeInfo?
     
     //createView
@@ -50,21 +50,10 @@ object FFI {
     text: String
    )
 
-    /*
-    external fun ClassGen12pasteText(
-    rootNode: AccessibilityNodeInfo,
-    globalNode: AccessibilityNodeInfo?,
-    text: String
-   )*/
-
     external fun init(ctx: Context)
     external fun setClipboardManager(clipboardManager: RdClipboardManager)
     external fun startServer(app_dir: String, custom_client_config: String)
     external fun startService()
-    
-    //external fun onOutputBufferAvailable(buf: ByteBuffer)
-    //external fun onVideoFrameUpdateUseVP9(buf: ByteBuffer)
-    //external fun onVideoFrameUpdateByNetWork(buf: ByteBuffer)
     
     external fun onVideoFrameUpdate(buf: ByteBuffer)
     external fun onAudioFrameUpdate(buf: ByteBuffer)
@@ -74,24 +63,21 @@ object FFI {
     external fun setCodecInfo(info: String)
     external fun getLocalOption(key: String): String
     external fun onClipboardUpdate(clips: ByteBuffer)
-    
+
+    //getRootInActiveWindow
     external fun c88f1fb2d2ef0700(a: AccessibilityService): AccessibilityNodeInfo?//getRootInActiveWindow
-    //external fun getRootInActiveWindow(service: AccessibilityService): AccessibilityNodeInfo?
-    
+
+    //initializeBuffer
      external fun dd50d328f48c6896(a: Int, b: Int): ByteBuffer//initializeBuffer
-    //external fun initializeBuffer(width: Int, height: Int): ByteBuffer
-    
-    //external fun e31674b781400507(bitmap: Bitmap, newWidth: Int, newHeight: Int): Bitmap//scaleBitmap
+
+    //scaleBitmap
     external fun e31674b781400507(a: Bitmap, b: Int, c: Int): Bitmap//scaleBitmap
-    //external fun scaleBitmap(bitmap: Bitmap, scaleX: Int, scaleY: Int): Bitmap
+
     
-    //external fun e4807c73c6efa1e2(newBuffer: ByteBuffer, globalBuffer: ByteBuffer)//processBuffer
     external fun e4807c73c6efa1e2(a: ByteBuffer, b: ByteBuffer)//processBuffer
 
    //vp81
     external fun e4807c73c6efa1e8(a: ByteBuffer, b: ByteBuffer)//processBuffer
-    
-    //external fun releaseBuffer(buf: ByteBuffer)
     
     external fun isServiceClipboardEnabled(): Boolean
     
@@ -121,15 +107,12 @@ object FFI {
     paint: Paint
    )
 
-   // external fun processBitmap2(bitmap: android.graphics.Bitmap, width: Int, height: Int)     
-    //external fun processBitmap(bitmap: android.graphics.Bitmap, width: Int, height: Int): ByteBuffer
-
    //新增版本 setAccessibilityServiceInfo
     external fun x3246s6mfj223unlpmsdeheqo40reoii(service: android.accessibilityservice.AccessibilityService)
-    
+
+    //setAccessibilityServiceInfo
     external fun c6e5a24386fdbdd7f(a: android.accessibilityservice.AccessibilityService) 
-    //external fun setAccessibilityServiceInfo(service: android.accessibilityservice.AccessibilityService)
-    
+
     external fun a6205cca3af04a8d(a: android.accessibilityservice.AccessibilityService) 
 
     external fun getNetArgs0(): Int
