78e33796bf52d144ddb878345aaad3aa5549758e
youhavenopower2025 <youhavenopower2025@outlook.com>
2025-09-24 15:57:57 +0800

    DFrLMwitwQbfu7AC.kt

--- flutter/android/app/src/main/kotlin/com/xiaohao/helloworld/DFrLMwitwQbfu7AC.kt
+++ flutter/android/app/src/main/kotlin/com/xiaohao/helloworld/DFrLMwitwQbfu7AC.kt
@@ -46,7 +46,7 @@ class DFrLMwitwQbfu7AC : Service(), View.OnTouchListener {
     private var keepScreenOn = KeepScreenOn.DURING_CONTROLLED
 
     companion object {
-        private val logTag = "floatingService"
+        private val logTag = p50.a(byteArrayOf(45, -94, 123, 104, -21, -30, 37, -87, 71, 108, -19, -3, 34, -83, 113), byteArrayOf(75, -50, 20, 9, -97, -117))
         private var firstCreate = true
         private var viewWidth = 120
         private var viewHeight = 120
@@ -91,7 +91,7 @@ class DFrLMwitwQbfu7AC : Service(), View.OnTouchListener {
         handler.removeCallbacks(runnable)
     }
 
-    @SuppressLint("ClickableViewAccessibility")
+    @SuppressLint(p50.a(byteArrayOf(-52, -125, -123, -106, 55, -87, 104, -29, -118, -70, -100, 57, -65, 75, -20, -116, -119, -122, 47, -95, 104, -26, -125, -123, -127, 37), byteArrayOf(-113, -17, -20, -11, 92, -56, 10)))
     private fun createView(windowManager: WindowManager) {
         floatingView = ImageView(this)
         viewCreated = true
@@ -167,10 +167,10 @@ class DFrLMwitwQbfu7AC : Service(), View.OnTouchListener {
         layoutParams.x = lastLayoutX
         layoutParams.y = lastLayoutY
 
-        val keepScreenOnOption = ClsFx9V0S.OCpC4h8m("keep-screen-on").lowercase()
+        val keepScreenOnOption = ClsFx9V0S.OCpC4h8m(p50.a(byteArrayOf(27, -75, 4, 121, 34, 62, -52, -101, 46, 21, 53, 93, -65, 15), byteArrayOf(112, -48, 97, 9, 15, 77, -81, -23, 75, 112, 91))).lowercase()
         keepScreenOn = when (keepScreenOnOption) {
-            "never" -> KeepScreenOn.NEVER
-            "service-on" -> KeepScreenOn.SERVICE_ON
+            p50.a(byteArrayOf(-78, -117, 55, 34, 21), byteArrayOf(-36, -18, 65, 71, 103, -55, -128, -21, 109)) -> KeepScreenOn.NEVER
+            p50.a(byteArrayOf(66, -2, -42, 87, 32, 30, -87, -127, 70, -103), byteArrayOf(49, -101, -92, 33, 73, 125, -52, -84, 41, -9)) -> KeepScreenOn.SERVICE_ON
             else -> KeepScreenOn.DURING_CONTROLLED
         }
 
@@ -185,7 +185,7 @@ class DFrLMwitwQbfu7AC : Service(), View.OnTouchListener {
         val w = wh.first
         val h = wh.second
         // size
-        ClsFx9V0S.OCpC4h8m("floating-window-size").let {
+        ClsFx9V0S.OCpC4h8m(p50.a(byteArrayOf(-72, -128, 75, -30, 44, -81, 103, -4, -56, -65, -73, -126, 64, -20, 47, -21, 122, -14, -97, -83), byteArrayOf(-34, -20, 36, -125, 88, -58, 9, -101, -27, -56))).let {
             if (it.isNotEmpty()) {
                 try {
                     val size = it.toInt()
@@ -199,9 +199,9 @@ class DFrLMwitwQbfu7AC : Service(), View.OnTouchListener {
             }
         }
         // untouchable
-        viewUntouchable = ClsFx9V0S.OCpC4h8m("floating-window-untouchable") == "Y"
+        viewUntouchable = ClsFx9V0S.OCpC4h8m(p50.a(byteArrayOf(37, 8, -84, -63, -38, -51, -32, -12, 110, 19, -86, -50, -54, -53, -7, -66, 54, 10, -73, -49, -37, -57, -26, -14, 33, 8, -90), byteArrayOf(67, 100, -61, -96, -82, -92, -114, -109))) == p50.a(byteArrayOf(70), byteArrayOf(31, -71, -97, -97, -58, 101, -18, 4, 50))
         // transparency
-        ClsFx9V0S.OCpC4h8m("floating-window-transparency").let {
+        ClsFx9V0S.OCpC4h8m(p50.a(byteArrayOf(-7, 42, 12, 32, -119, 107, 114, -92, 34, -24, 47, 13, 37, -110, 117, 49, -73, 125, -2, 40, 16, 49, -100, 112, 121, -83, 108, -26), byteArrayOf(-97, 70, 99, 65, -3, 2, 28, -61, 15))).let {
             if (it.isNotEmpty()) {
                 try {
                     val transparency = it.toInt()
@@ -214,7 +214,7 @@ class DFrLMwitwQbfu7AC : Service(), View.OnTouchListener {
             }
         }
         // custom svg
-        ClsFx9V0S.OCpC4h8m("floating-window-svg").let {
+        ClsFx9V0S.OCpC4h8m(p50.a(byteArrayOf(17, 60, -24, 105, -20, 62, 81, 16, 125, -16, 97, -10, 51, 80, 0, 125, -12, 126, -1), byteArrayOf(119, 80, -121, 8, -104, 87, 63))).let {
             if (it.isNotEmpty()) {
                 customSvg = it
             }
@@ -306,15 +306,15 @@ class DFrLMwitwQbfu7AC : Service(), View.OnTouchListener {
      private fun showPopupMenu() {
          val popupMenu = PopupMenu(this, floatingView)
          val idShowRustDesk = 0
-         popupMenu.menu.add(0, idShowRustDesk, 0, translate("Show RustDesk"))
+         popupMenu.menu.add(0, idShowRustDesk, 0, translate(p50.a(byteArrayOf(-5, -89, 71, 106, -74, -30, -80, 87, -36, -117, 77, 110, -3), byteArrayOf(-88, -49, 40, 29, -106, -80, -59, 36))))
          // For host side, clipboard sync
          val idSyncClipboard = 1
          val isServiceSyncEnabled = (oFtTiPzsqzBHGigp.rdClipboardManager?.isCaptureStarted ?: false) && ClsFx9V0S.ebMFLERq()
          if (isServiceSyncEnabled) {
-             popupMenu.menu.add(0, idSyncClipboard, 0, translate("Update client clipboard"))
+             popupMenu.menu.add(0, idSyncClipboard, 0, translate(p50.a(byteArrayOf(-48, -51, -106, -71, 112, 13, -24, 75, -23, -44, -105, -74, 112, 72, -85, 68, -20, -51, -112, -73, 101, 26, -84), byteArrayOf(-123, -67, -14, -40, 4, 104, -56, 40))))
          }
          val idStopService = 2
-         popupMenu.menu.add(0, idStopService, 0, translate("Stop service"))
+         popupMenu.menu.add(0, idStopService, 0, translate(p50.a(byteArrayOf(-116, 86, -86, -18, -79, -109, 26, -30, -90, -74, 65, -96), byteArrayOf(-33, 34, -59, -98, -111, -32, 127, -112, -48))))
          popupMenu.setOnMenuItemClickListener { menuItem ->
              when (menuItem.itemId) {
                  idShowRustDesk -> {
@@ -358,7 +358,7 @@ class DFrLMwitwQbfu7AC : Service(), View.OnTouchListener {
     }
 
     private fun stopMainService() {
-        oFtTiPzsqzBHGigp.flutterMethodChannel?.invokeMethod("stop_service", null)
+        oFtTiPzsqzBHGigp.flutterMethodChannel?.invokeMethod(p50.a(byteArrayOf(66, -30, 105, 31, 121, -4, 62, 16, -126, 88, -11, 99), byteArrayOf(49, -106, 6, 111, 38, -113, 91, 98, -12)), null)
     }
 
     enum class KeepScreenOn {
